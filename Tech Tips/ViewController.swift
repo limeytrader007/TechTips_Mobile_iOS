473ab0457306af6203c0f165500eacd302867703
Daniel Chapman <danielgchap@hotmail.com>
2016-08-26 20:56:12 -0400

    Initial Commit

---
+++ Tech Tips/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Tech Tips
+//
+//  Created by Daniel Chapman on 8/26/16.
+//  Copyright © 2016 Daniel Chapman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
